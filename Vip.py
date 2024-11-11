f7e1942689db622e7939b2afb0e8e9b16cccccf3
Donehhhhhh <comddr33@gmail.com>
2024-11-11 14:53:58 +0530

    Add files via upload

---
+++ Vip.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Vip_Ddos_07
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7358755762:AAH8nEpfFdZuKm8eSW96pCd7uJEgroKJIn4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("DONNEHHH")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6286507682")
+
+
+
+
+
+  
